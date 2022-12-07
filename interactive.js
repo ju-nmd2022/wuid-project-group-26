85e45d8cf21d61aa1108532af779037b9d1f5129
wilmahjortas <hjwi22oe@student.ju.se>
2022-12-07 18:24:17 +0100

    wilmas commit

--- interactive.js
+++ interactive.js
@@ -1,5 +1,17 @@
 // Function provided by Codegrid on YouTube
 
 function show() {
-    document.getElementById('sidebar').classList.toggle('active');
-  }
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
+//help from https://dirask.com/posts/JavaScript-change-image-on-click-1wAYZp
+
+var image = document.querySelector(".image");
+
+image.addEventListener("click", function () {
+  image.src = "heart-solid.svg";
+});
+
+function changeImage(image) {
+  image.src = "heart-solid.svg";
+}
