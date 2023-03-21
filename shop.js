415ab2faadd08ce90432643ed6b1688b0e31285b
wilmahjortas <hjwi22oe@student.ju.se>
2023-03-21 11:13:40 +0100

    wilma new commit

--- shop.js
+++ shop.js
@@ -1,3 +1,14 @@
+//new
+let popup = document.getElementById("popup");
+
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
+}
+
 //help from https://dirask.com/posts/JavaScript-change-image-on-click-1wAYZp
 
 var image = document.querySelector(".image");
